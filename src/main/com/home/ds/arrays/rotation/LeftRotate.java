708ed0acdcb2c2b1c6f6b5493484b87483bc4f53
Gaurav Rehan <gaurav_rehan@hotmail.com>
2019-09-02 22:43:08 +0530

    Array rotation - Juggle algorithm - fix the bug with circular indexing

--- src/main/com/home/ds/arrays/rotation/LeftRotate.java
+++ src/main/com/home/ds/arrays/rotation/LeftRotate.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class LeftRotate {
 
-    static int[] a = new int[]{1,2,3,4,5,6};
+    static int[] a = new int[]{1,2,3,4,5};
 
     public static void main(String[] args)
     {
         System.out.println(Arrays.toString(a));
-        new LeftRotate().juggleAlgorithm(a, 6, 2, 2);
+        new LeftRotate().juggleAlgorithm(a, 5, 4, 1);
         System.out.println(Arrays.toString(a));
     }
 
@@ -22,9 +22,8 @@ public class LeftRotate {
             int j = i;
 
             while(true) {
-
-                int d = (j + k);
-                if (d < n)
+                int d = (j + k) % n;
+                if (d != i)
                 {
                     a[j] = a[d];
                     j = d;
@@ -37,6 +36,4 @@ public class LeftRotate {
             }
         }
     }
-
-
 }
