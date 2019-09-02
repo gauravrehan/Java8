f3ddc91d33ac8cc8ac8e0f3be6bf60eb20927ac2
Gaurav Rehan <gaurav_rehan@hotmail.com>
2019-09-02 22:56:00 +0530

    Array rotation - Juggle algorithm - documentation

--- src/main/com/home/ds/arrays/rotation/LeftRotate.java
+++ src/main/com/home/ds/arrays/rotation/LeftRotate.java
@@ -2,6 +2,15 @@ package com.home.ds.arrays.rotation;
 
 import java.util.Arrays;
 
+/*
+Juggle Algorithm to rotate the array k positions to left with space complexity as O(1)
+Time complexity is O(n)
+n = array length
+k = positions to rotate to left
+gcd = greatest common divisor. for example gcd of 7 and 1 is 1. and 7 and 7 is 7. 7 and 2 is 1.
+outer loops goes from 0 to < gcd
+inner loop starts at i and terminate at i.
+ */
 public class LeftRotate {
 
     static int[] a = new int[]{1,2,3,4,5};
