d88147b8759c1d97d4f64d387edc2a67c5eb1c3e
Gaurav Rehan <gaurav.rehan@gmail.com>
2022-01-08 20:02:00 +0530

    Comparator comparing thenComparing

--- src/main/com/home/apijava8/comparator/MainComparator.java
+++ src/main/com/home/apijava8/comparator/MainComparator.java
@@ -1,11 +1,24 @@
 package com.home.apijava8.comparator;
 
 
+import java.util.function.Function;
+
 public class MainComparator {
 
     public static void main(String... args)
     {
 
+        Comparator<Person> cmpAge = (person1, person2) -> person1.getAge() - person2.getAge();
+        Comparator<Person> cmpFirstName = (person1, person2) -> person1.getFirstName().compareTo(person2.getFirstName());
+        Comparator<Person> cmpLastName = (person1, person2) -> person1.getLastName().compareTo(person2.getLastName());
+
+
+
+        Comparator<Person> cmpPersonfAge = Comparator.comparing(Person::getAge);
+        Comparator<Person> cmpPersonfFirstName = Comparator.comparing(Person::getFirstName);
+
+        //Comparator<Person> cmpPersonMultiple = cmpPersonfAge.thenComparing(cmpPersonfFirstName);
+        Comparator<Person> cmp= Comparator.comparing(Person::getAge).thenComparing(Person::getLastName).thenComparing(Person::getFirstName);
 
     }
 }
